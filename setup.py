537f99b61a376c24ed8598359994af427a21681c
Alex Reustle <areustledev@gmail.com>
2020-12-13 13:46:00 -0500

    Fix version parsing

--- setup.py
+++ setup.py
@@ -5,11 +5,8 @@ nuSpaceSim
 A Simulator for Space-based Neutrino Detections.
 """
 import ast
-import re
 from setuptools import setup
 from pybind11.setup_helpers import Pybind11Extension, build_ext
-
-_version_re = re.compile(r"__version__\s+=\s+(.*)")
 
 with open("nuSpaceSim/__init__.py", "rb") as f:
     version = str(
@@ -21,7 +18,6 @@ zsteps = Pybind11Extension("nuSpaceSim.EAScherGen.zsteps",
                            ["nuSpaceSim/EAScherGen/src/zsteps.cpp"])
 
 setup(
-    version=version,
     cmdclass={"build_ext": build_ext},
     ext_modules=[nssgeometry, zsteps],
 )
