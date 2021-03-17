714e318e5a7e0dac4a0336b6009a041087cfab9b
Alex Reustle <areustledev@gmail.com>
2021-03-17 14:29:04 -0400

    Smarter version string management

--- nuSpaceSim/__init__.py
+++ nuSpaceSim/__init__.py
@@ -1,6 +1,2 @@
-import subprocess
-sha = subprocess.getoutput("git rev-parse --short HEAD").strip()
-
-major_minor_patch = '0.1.3'
-
-__version__ = f'{major_minor_patch}'
+from version_query import predict_version_str
+__version__ = predict_version_str()
