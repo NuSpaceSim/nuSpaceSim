0ea10d3af2c1390f7d8adf6112d245e86f53d0d7
Alex Reustle <areustledev@gmail.com>
2021-11-29 22:02:25 -0500

    rg updates

--- src/nuspacesim/simulation/geometry/rg.py
+++ src/nuspacesim/simulation/geometry/rg.py
@@ -1,188 +1,186 @@
 import numpy as np
 
 
-def geo_dmc(u, config):
+class Geo:
+    def __init__(self, config):
+        self.config = config
+        radE = config.constants.earth_radius
+        detalt = config.detector.altitude
+        detra = config.detector.ra_start
+        detdec = config.detector.dec_start
+        delAlpha = config.simulation.ang_from_limb
+        maxsepangle = config.simulation.theta_ch_max
+        delAziAng = config.simulation.max_azimuth_angle
 
-    radE = config.constants.earth_radius
-    detalt = config.detector.altitude
-    detra = config.detector.ra_start
-    detdec = config.detector.dec_start
-    delAlpha = config.simulation.ang_from_limb
-    maxsepangle = config.simulation.theta_ch_max
-    delAziAng = config.simulation.max_azimuth_angle
+        self.earth_radius = radE
+        self.earth_rad_2 = self.earth_radius * self.earth_radius
 
-    earthRadius = radE
-    earthRadiusSqrd = earthRadius * earthRadius
+        self.detAlt = detalt
+        self.radEplusDetAlt = self.earth_radius + self.detAlt
+        self.radEplusDetAltSqrd = self.radEplusDetAlt * self.radEplusDetAlt
 
-    detAlt = detalt
-    radEplusDetAlt = earthRadius + detAlt
-    radEplusDetAltSqrd = radEplusDetAlt * radEplusDetAlt
+        self.detRA = np.radians(detra)
+        self.detDec = np.radians(detdec)
 
-    detRA = np.radians(detra)
-    detDec = np.radians(detdec)
+        alphaHorizon = 0.5 * np.pi - np.arccos(self.earth_radius / self.radEplusDetAlt)
+        alphaMin = alphaHorizon - delAlpha
+        minChordLen = 2.0 * np.sqrt(
+            self.earth_rad_2
+            - self.radEplusDetAltSqrd * np.sin(alphaMin) * np.sin(alphaMin)
+        )
 
-    alphaHorizon = 0.5 * np.pi - np.arccos(earthRadius / radEplusDetAlt)
-    alphaMin = alphaHorizon - delAlpha
-    minChordLen = 2.0 * np.sqrt(
-        earthRadiusSqrd - radEplusDetAltSqrd * np.sin(alphaMin) * np.sin(alphaMin)
-    )
+        self.minLOSpathLen = self.radEplusDetAlt * np.cos(alphaMin) - 0.5 * minChordLen
 
-    minLOSpathLen = radEplusDetAlt * np.cos(alphaMin) - 0.5 * minChordLen
-    # minLOSpathLenSqrd = minLOSpathLen * minLOSpathLen
-    minLOSpathLenCubed = minLOSpathLen * minLOSpathLen * minLOSpathLen
+        self.maxThetaS = np.arccos(self.earth_radius / self.radEplusDetAlt)
+        self.minThetaS = np.arccos(
+            (self.radEplusDetAltSqrd + self.earth_rad_2 - self.minLOSpathLen ** 2)
+            * 1.0
+            / (2.0 * self.radEplusDetAlt * self.earth_radius)
+        )
 
-    # maxThetaS = np.arccos(earthRadius / radEplusDetAlt)
-    # minThetaS = np.arccos(
-    #     (radEplusDetAltSqrd + earthRadiusSqrd - minLOSpathLenSqrd)
-    #     * 1.0
-    #     / (2.0 * radEplusDetAlt * earthRadius)
-    # )
-    # cosOfMaxThetaS = np.cos(maxThetaS)
-    # cosOfMinThetaS = np.cos(minThetaS)
+        self.maxLOSpathLen = np.sqrt(self.radEplusDetAltSqrd - self.earth_rad_2)
+        self.maxLOSpathLenCubed = self.maxLOSpathLen ** 3
 
-    maxLOSpathLen = np.sqrt(radEplusDetAltSqrd - earthRadiusSqrd)
-    # maxLOSpathLenSqrd = maxLOSpathLen * maxLOSpathLen
-    maxLOSpathLenCubed = maxLOSpathLen * maxLOSpathLen * maxLOSpathLen
+        self.maxThetaTrSubV = maxsepangle
+        self.sinOfMaxThetaTrSubV = np.sin(self.maxThetaTrSubV)
 
-    maxThetaTrSubV = maxsepangle
-    sinOfMaxThetaTrSubV = np.sin(maxThetaTrSubV)
+        self.maxPhiS = 0.5 * delAziAng
+        self.minPhiS = -0.5 * delAziAng
 
-    maxPhiS = 0.5 * delAziAng
-    minPhiS = -0.5 * delAziAng
+        self.normThetaTrSubV = 2.0 / (
+            self.sinOfMaxThetaTrSubV * self.sinOfMaxThetaTrSubV
+        )
+        self.normPhiTrSubV = 1.0 / (2.0 * np.pi)
+        self.normPhiS = 1.0 / (self.maxPhiS - self.minPhiS)
 
-    normThetaTrSubV = 2.0 / (sinOfMaxThetaTrSubV * sinOfMaxThetaTrSubV)
-    normPhiTrSubV = 1.0 / (2.0 * np.pi)
-    normPhiS = 1.0 / (maxPhiS - minPhiS)
+        bracketForNormThetaS = (
+            (self.radEplusDetAltSqrd - self.earth_rad_2) * self.maxLOSpathLen
+            - (1.0 / 3.0) * self.maxLOSpathLen ** 3
+            - (self.radEplusDetAltSqrd - self.earth_rad_2) * self.minLOSpathLen
+            + (1.0 / 3.0) * self.minLOSpathLen ** 3
+        )
 
-    bracketForNormThetaS = (
-        (radEplusDetAltSqrd - earthRadiusSqrd) * maxLOSpathLen
-        - (1.0 / 3.0) * maxLOSpathLenCubed
-        - (radEplusDetAltSqrd - earthRadiusSqrd) * minLOSpathLen
-        + (1.0 / 3.0) * minLOSpathLenCubed
-    )
+        normThetaS = 2.0 * self.radEplusDetAlt * self.earth_rad_2 / bracketForNormThetaS
 
-    normThetaS = 2.0 * radEplusDetAlt * earthRadiusSqrd / bracketForNormThetaS
+        pdfnorm = self.normThetaTrSubV * self.normPhiTrSubV * self.normPhiS * normThetaS
+        self.mcnorm = self.earth_rad_2 / pdfnorm
 
-    pdfnorm = normThetaTrSubV * normPhiTrSubV * normPhiS * normThetaS
-    mcnorm = earthRadiusSqrd / pdfnorm
+    def throw(self, u):
+        """Throw N events with 4 * u random numbers"""
 
-    # u1, u2, u3, u4 = u
-    u1 = u[:, 0]
-    u2 = u[:, 1]
-    u3 = u[:, 2]
-    u4 = u[:, 3]
-    N = u.shape[0]
+        # u1, u2, u3, u4 = u
+        u1 = u[:, 0]
+        u2 = u[:, 1]
+        u3 = u[:, 2]
+        u4 = u[:, 3]
+        N = u.shape[0]
 
-    rthetaTrSubV = np.arcsin(sinOfMaxThetaTrSubV * np.sqrt(u1))
-    rcosthetaTrSubV = np.cos(rthetaTrSubV)
-    rphiTrSubV = 2.0 * np.pi * u2
-    rphiS = (maxPhiS - minPhiS) * u3 + minPhiS
+        self.thetaTrSubV = np.arcsin(self.sinOfMaxThetaTrSubV * np.sqrt(u1))
+        costhetaTrSubV = np.cos(self.thetaTrSubV)
+        phiTrSubV = 2.0 * np.pi * u2
+        phiS = (self.maxPhiS - self.minPhiS) * u3 + self.minPhiS
 
-    # Generate theta_s (the colatitude on the surface of the Earth in the
-    # detector nadir perspective)
+        # Generate theta_s (the colatitude on the surface of the Earth in the
+        # detector nadir perspective)
 
-    b = (
-        3.0 * (radEplusDetAltSqrd - earthRadiusSqrd) * maxLOSpathLen
-        - maxLOSpathLenCubed
-        - 3.0 * (radEplusDetAltSqrd - earthRadiusSqrd) * minLOSpathLen
-        + minLOSpathLenCubed
-    )
-    q = -1.0 * (radEplusDetAltSqrd - earthRadiusSqrd)
-    r = (
-        -1.5 * (radEplusDetAltSqrd - earthRadiusSqrd) * maxLOSpathLen
-        + 0.5 * maxLOSpathLenCubed
-        + 0.5 * b * u4
-    )
+        b = (
+            3.0 * (self.radEplusDetAltSqrd - self.earth_rad_2) * self.maxLOSpathLen
+            - self.maxLOSpathLen ** 3
+            - 3.0 * (self.radEplusDetAltSqrd - self.earth_rad_2) * self.minLOSpathLen
+            + self.minLOSpathLen ** 3
+        )
+        q = -1.0 * (self.radEplusDetAltSqrd - self.earth_rad_2)
+        r = (
+            -1.5 * (self.radEplusDetAltSqrd - self.earth_rad_2) * self.maxLOSpathLen
+            + 0.5 * self.maxLOSpathLenCubed
+            + 0.5 * b * u4
+        )
 
-    psi = np.arccos(r / np.sqrt(-1.0 * q * q * q))
-    v1 = 2.0 * np.sqrt(-1.0 * q) * np.cos(psi / 3.0)
-    v2 = 2.0 * np.sqrt(-1.0 * q) * np.cos((psi + 2.0 * np.pi) / 3.0)
-    v3 = 2.0 * np.sqrt(-1.0 * q) * np.cos((psi + 4.0 * np.pi) / 3.0)
+        psi = np.arccos(r / np.sqrt(-1.0 * q * q * q))
+        v1 = 2.0 * np.sqrt(-1.0 * q) * np.cos(psi / 3.0)
+        v2 = 2.0 * np.sqrt(-1.0 * q) * np.cos((psi + 2.0 * np.pi) / 3.0)
+        v3 = 2.0 * np.sqrt(-1.0 * q) * np.cos((psi + 4.0 * np.pi) / 3.0)
 
-    discriminant = q * q * q + r * r
+        dscr = q * q * q + r * r
 
-    d_mask = discriminant <= 0
-    v1_msk = d_mask & (v1 > 0) & (v1 >= minLOSpathLen) & (v1 <= maxLOSpathLen)
-    v2_msk = d_mask & (v2 > 0) & (v2 >= minLOSpathLen) & (v2 <= maxLOSpathLen)
-    v3_msk = d_mask & (v3 > 0) & (v3 >= minLOSpathLen) & (v3 <= maxLOSpathLen)
+        d_mask = dscr <= 0
+        v1_msk = (
+            d_mask & (v1 > 0) & (v1 >= self.minLOSpathLen) & (v1 <= self.maxLOSpathLen)
+        )
+        v2_msk = (
+            d_mask & (v2 > 0) & (v2 >= self.minLOSpathLen) & (v2 <= self.maxLOSpathLen)
+        )
+        v3_msk = (
+            d_mask & (v3 > 0) & (v3 >= self.minLOSpathLen) & (v3 <= self.maxLOSpathLen)
+        )
 
-    rlosPathLen = np.zeros_like(v1)
-    rlosPathLen[v1_msk] = v1[v1_msk]
-    rlosPathLen[v2_msk] = v2[v2_msk]
-    rlosPathLen[v3_msk] = v3[v3_msk]
+        losPathLen = np.zeros_like(v1)
+        losPathLen[v1_msk] = v1[v1_msk]
+        losPathLen[v2_msk] = v2[v2_msk]
+        losPathLen[v3_msk] = v3[v3_msk]
 
-    rlosPathLen[~d_mask] = np.sum(
-        np.cbrt(
-            r[~d_mask] + np.multiply.outer([1, -1], np.sqrt(discriminant[~d_mask]))
-        ),
-        axis=0,
-    )
+        losPathLen[~d_mask] = np.sum(
+            np.cbrt(r[~d_mask] + np.multiply.outer([1, -1], np.sqrt(dscr[~d_mask]))),
+            axis=0,
+        )
 
-    rvsqrd = rlosPathLen * rlosPathLen
-    rcosthetaS = (radEplusDetAltSqrd + earthRadiusSqrd - rvsqrd) / (
-        2.0 * earthRadius * radEplusDetAlt
-    )
-    rthetaS = np.arccos(rcosthetaS)
+        rvsqrd = losPathLen * losPathLen
+        costhetaS = (self.radEplusDetAltSqrd + self.earth_rad_2 - rvsqrd) / (
+            2.0 * self.earth_radius * self.radEplusDetAlt
+        )
+        thetaS = np.arccos(costhetaS)
 
-    rcosthetaNSubV = (radEplusDetAltSqrd - earthRadiusSqrd - rvsqrd) / (
-        2.0 * earthRadius * rlosPathLen
-    )
+        costhetaNSubV = (self.radEplusDetAltSqrd - self.earth_rad_2 - rvsqrd) / (
+            2.0 * self.earth_radius * losPathLen
+        )
 
-    rthetaNSubV = np.arccos(rcosthetaNSubV)
+        thetaNSubV = np.arccos(costhetaNSubV)
 
-    rcosthetaTrSubN = np.cos(rthetaTrSubV) * rcosthetaNSubV - np.sin(
-        rthetaTrSubV
-    ) * np.sin(rthetaNSubV) * np.cos(rphiTrSubV)
+        self.costhetaTrSubN = np.cos(self.thetaTrSubV) * costhetaNSubV - np.sin(
+            self.thetaTrSubV
+        ) * np.sin(thetaNSubV) * np.cos(phiTrSubV)
 
-    rthetaTrSubN = np.arccos(rcosthetaTrSubN)
+        thetaTrSubN = np.arccos(self.costhetaTrSubN)
 
-    # if (rcosthetaTrSubN < 0.0)
-    #   geoKeep = False; // Trajectories going into the ground
+        # if (rcosthetaTrSubN < 0.0)
+        #   geoKeep = False; // Trajectories going into the ground
 
-    rbetaTrSubN = np.degrees(0.5 * np.pi - rthetaTrSubN)
+        self.betaTrSubN = np.degrees(0.5 * np.pi - thetaTrSubN)
+        rsindecS = np.sin(self.config.detector.dec_start) * costhetaS - np.cos(
+            self.config.detector.dec_start
+        ) * np.sin(thetaS) * np.cos(phiS)
+        self.rdecS = np.degrees(np.arcsin(rsindecS))
 
-    rsindecS = np.sin(detDec) * rcosthetaS - np.cos(detDec) * np.sin(rthetaS) * np.cos(
-        rphiS
-    )
-    rdecS = np.degrees(np.arcsin(rsindecS))
+        self.rxS = (
+            np.sin(self.config.detector.dec_start)
+            * np.cos(self.config.detector.ra_start)
+            * np.sin(thetaS)
+            * np.cos(phiS)
+            - np.sin(self.config.detector.ra_start) * np.sin(thetaS) * np.sin(phiS)
+            + np.cos(self.config.detector.dec_start)
+            * np.cos(self.config.detector.ra_start)
+            * np.cos(thetaS)
+        )
 
-    rxS = (
-        np.sin(detDec) * np.cos(detRA) * np.sin(rthetaS) * np.cos(rphiS)
-        - np.sin(detRA) * np.sin(rthetaS) * np.sin(rphiS)
-        + np.cos(detDec) * np.cos(detRA) * np.cos(rthetaS)
-    )
+        self.ryS = (
+            np.sin(self.config.detector.dec_start)
+            * np.sin(self.config.detector.ra_start)
+            * np.sin(thetaS)
+            * np.cos(phiS)
+            + np.cos(self.config.detector.ra_start) * np.sin(thetaS) * np.sin(phiS)
+            + np.cos(self.config.detector.dec_start)
+            * np.sin(self.config.detector.ra_start)
+            * np.cos(thetaS)
+        )
 
-    ryS = (
-        np.sin(detDec) * np.sin(detRA) * np.sin(rthetaS) * np.cos(rphiS)
-        + np.cos(detRA) * np.sin(rthetaS) * np.sin(rphiS)
-        + np.cos(detDec) * np.sin(detRA) * np.cos(rthetaS)
-    )
+        self.rraS = np.degrees(np.arctan2(self.ryS, self.rxS))
+        self.rraS[self.rraS < 0.0] += 360.0
 
-    rraS = np.degrees(np.arctan2(ryS, rxS))
-    rraS[rraS < 0.0] += 360.0
+        self.event_mask = self.costhetaTrSubN >= 0
 
-    good_event = rcosthetaTrSubN >= 0
-
-    good = rcosthetaTrSubV >= np.cos(np.radians(1.5))
-    geom_factor = np.sum(
-        rcosthetaTrSubN / (rcosthetaNSubV * rcosthetaTrSubV), where=(good_event & good)
-    )
-    geom_factor *= np.reciprocal(N) * mcnorm
-
-    return (
-        geom_factor,
-        good_event,
-        rthetaS,
-        rphiS,
-        rraS,
-        rdecS,
-        rthetaTrSubV,
-        rcosthetaTrSubV,
-        rphiTrSubV,
-        rthetaTrSubN,
-        rcosthetaTrSubN,
-        rbetaTrSubN,
-        rlosPathLen,
-        rthetaNSubV,
-        rcosthetaNSubV,
-    )
+        good = costhetaTrSubV >= np.cos(np.radians(1.5))
+        self.geom_factor = np.sum(
+            self.costhetaTrSubN / (costhetaNSubV * costhetaTrSubV),
+            where=(self.event_mask & good),
+        )
+        self.geom_factor *= np.reciprocal(N) * self.mcnorm
--- test/simulation/geometry/test_rg.py
+++ test/simulation/geometry/test_rg.py
@@ -7,18 +7,24 @@ def test_rg():
 
     cfg = NssConfig()
 
-    u = np.random.uniform(0, 1.0, (int(1e6), 4))
-    t = rg.geo_dmc(u, cfg)
+    u = np.float32(np.random.rand(int(1e6), 4))
+    T = rg.Geo(cfg)
+    T.throw(u)
 
     R = region_geometry.RegionGeom(cfg)
     R.run_geo_dmc_from_ran_array_nparray(u)
 
-    assert all(R.evMasknpArray == t[1])
+    assert all(R.evMasknpArray == T.event_mask)
 
-    # Rmsk = R.evMasknpArray
-    # tmsk = t[1]
+    Rmsk = R.evMasknpArray
+    Tmsk = T.event_mask
 
-    # assert all(R.evArray["betaTrSubN"][Rmsk] == t[1])
+    # d = np.abs(R.evArray["betaTrSubN"][Rmsk]- T.betaTrSubN[Tmsk])
+    # i = np.argmax(d)
+    # print(d[i], R.evArray["betaTrSubN"][Rmsk][i], T.betaTrSubN[Tmsk][i])
+    assert np.allclose(
+        R.evArray["betaTrSubN"][Rmsk], T.betaTrSubN[Tmsk], rtol=1e-2, atol=1e-2
+    )
 
 
 if __name__ == "__main__":
