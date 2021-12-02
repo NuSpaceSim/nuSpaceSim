Fixed normalization issue in power-law spectrum implementation

src/nuspacesim/compute.py
@@ -166,8 +166,13 @@ def compute(
         f"\t[blue]Threw {config.simulation.N} neutrinos. {beta_tr.size} were valid.[/]"
     )
     logv("Computing [green] Energy Spectra.[/]")
-    log_e_nu, mc_spec_norm = spec(beta_tr.shape[0], store=sw, plot=to_plot)
+    #log_e_nu, SpecPdfValues, mc_spec_norm = spec(beta_tr.shape[0], store=sw, plot=to_plot)
+    spec_values, mc_spec_norm, SpecWeightsSum = spec(beta_tr.shape[0], store=sw, plot=to_plot)
+    log_e_nu = spec_values[0]
+    SpecPdfValues = spec_values[1]
     #spec_norm = 1.0
+
+    #print (spec_values, mc_spec_norm)
 
     logv("Computing [green] Taus.[/]")
     tauBeta, tauLorentz, showerEnergy, tauExitProb = tau(
@@ -194,7 +199,9 @@ def compute(
             costhetaChEff,
             tauExitProb,
             config.detector.photo_electron_threshold,
+            SpecPdfValues,
             mc_spec_norm,
+            SpecWeightsSum,
             "Mono",
         )
 
@@ -221,7 +228,7 @@ def compute(
 
         logv("Computing [green] Radio Monte Carlo Integral.[/]")
         mcint, mcintgeo, passEV = geom.mcintegral(
-            snrs, np.cos(thetaArr), tauExitProb, config.detector.det_SNR_thres, mc_spec_norm, "Mono"
+            snrs, np.cos(thetaArr), tauExitProb, config.detector.det_SNR_thres, SpecPdfValues, mc_spec_norm, SpecWeightsSum, "Mono"
         )
 
         sw.add_meta("RMCINT", mcint, "Radio MonteCarlo Integral")

src/nuspacesim/simulation/spectra/spectra.py
@@ -42,7 +42,7 @@ from .local_plots import spectra_histogram
 
 
 @decorators.nss_result_plot(spectra_histogram)
-@decorators.nss_result_store("log_e_nu")
+@decorators.nss_result_store("log_e_nu", "spectrum_values")
 def energy_spectra(
     N: int,
     spectra: Union[MonoSpectrum, PowerSpectrum, FileSpectrum, Callable],
@@ -52,7 +52,8 @@ def energy_spectra(
     """Energy Spectra of thrown Neutrinos"""
 
     if isinstance(spectra, MonoSpectrum):
-        return np.full(shape=(N), fill_value=spectra.log_nu_tau_energy)
+        #return np.full(shape=(N), fill_value=spectra.log_nu_tau_energy)
+        return np.full(shape=(N), fill_value=spectra.log_nu_tau_energy), np.ones(shape=(N))
 
     if isinstance(spectra, PowerSpectrum):
         p = spectra.index
@@ -60,7 +61,11 @@ def energy_spectra(
         b = 10 ** spectra.upper_bound
         mp = 1 - p
         u = np.random.uniform(0.0, 1.0 + np.finfo(np.float64).eps, size=N)
-        return np.reciprocal(mp) * np.log10(u * (b ** mp - a ** mp) + a ** mp)
+        log_e_nu = np.reciprocal(mp) * np.log10(u * (b ** mp - a ** mp) + a ** mp)
+        e_nu = 10 ** log_e_nu
+        pdf_value = e_nu ** (-1.*p)
+        #return np.reciprocal(mp) * np.log10(u * (b ** mp - a ** mp) + a ** mp)
+        return log_e_nu, pdf_value
 
     if isinstance(spectra, Callable):
         return spectra(*args, size=N, **kwargs)
@@ -68,7 +73,7 @@ def energy_spectra(
     else:
         raise RuntimeError(f"Spectra type not recognized {type(Spectra)}")
 
-def spec_norm(
+def spec_pdf_norm(
     spectra: Union[MonoSpectrum, PowerSpectrum, FileSpectrum, Callable],
     *args,
     **kwargs,
@@ -83,6 +88,39 @@ def spec_norm(
          b = 10 ** spectra.upper_bound
          mp = 1 - p
          return mp / (b ** mp - a ** mp)
+         #return (b ** mp - a ** mp) / mp
+
+def sum_spec_weights(
+    spectra: Union[MonoSpectrum, PowerSpectrum, FileSpectrum, Callable],
+    *args,
+    **kwargs,
+) -> float:
+
+     if isinstance(spectra, MonoSpectrum):
+         return 1.0
+
+     if isinstance(spectra, PowerSpectrum):
+         p = spectra.index
+         a = 10 ** spectra.lower_bound
+         b = 10 ** spectra.upper_bound
+         mp = 1 - p
+         #return mp / (b ** mp - a ** mp)
+         return (b ** mp - a ** mp) / mp
+
+#def spec_pdf_value(
+#    log_e_nu: float,
+#    spectra: Union[MonoSpectrum, PowerSpectrum, FileSpectrum, Callable],
+#    *args,
+#    **kwargs,
+#) -> float:
+
+#    if isinstance(spectra, MonoSpectrum):
+#        return 1.0
+
+#    if isinstance(spectra, PowerSpectrum):
+#        e_nu = 10 ** log_e_nu
+#        p = -1.*spectra.index
+#        return e_nu ** p
 
 class Spectra:
     """Energy Spectra of thrown Neutrinos"""
@@ -91,4 +129,4 @@ class Spectra:
         self.config = config
 
     def __call__(self, N, *args, **kwargs):
-        return energy_spectra(N, self.config.simulation.spectrum, *args, **kwargs), spec_norm(self.config.simulation.spectrum, *args, **kwargs)
+        return energy_spectra(N, self.config.simulation.spectrum, *args, **kwargs), spec_pdf_norm(self.config.simulation.spectrum, *args, **kwargs), sum_spec_weights(self.config.simulation.spectrum, *args, **kwargs)