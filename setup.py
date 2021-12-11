ed220e1380245873ca17165761e1c9dd2052e635
Alex Reustle <Areustle@users.noreply.github.com>
2021-12-11 12:56:09 -0500

    Add support for python 3.10 (#19)
    * Add support for python 3.10
    * update cibuildwheel version to 2.3.0

--- setup.py
+++ setup.py
@@ -8,15 +8,9 @@ A Simulator for Space-based Neutrino Detections.
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
 
-# nssgeometry = Pybind11Extension(
-#     "simulation.geometry.nssgeometry",
-#     ["test/simulation/geometry/src/nssgeometry.cpp"],
-# )
-
 zsteps = Pybind11Extension(
     "simulation.eas_optical.zsteps",
     ["src/nuspacesim/simulation/eas_optical/src/zsteps.cpp"],
 )
 
-# setup(cmdclass={"build_ext": build_ext}, ext_modules=[nssgeometry, zsteps])
 setup(cmdclass={"build_ext": build_ext}, ext_modules=[zsteps])
