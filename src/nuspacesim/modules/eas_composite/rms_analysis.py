db6519a81c193fd69059dddf593fa7229d90ed7d
Fred <fgarcia4@umd.edu>
2022-02-10 01:38:39 -0500

    mc rms

--- src/nuspacesim/modules/eas_composite/rms_analysis.py
+++ src/nuspacesim/modules/eas_composite/rms_analysis.py
@@ -23,7 +23,7 @@ trimmed_showers_00km, test_depths = make_composites_00km.shower_end_cuts(
 
 plt.figure(figsize=(8,6), dpi=200)  
 bin_00km, mean_00km, rms_low, rms_high = mean_rms_plot(
-    trimmed_showers_00km,comp_depths_00km, label='15 km', facecolor='tab:red'
+    trimmed_showers_00km,comp_depths_00km, label='0 km', facecolor='tab:red'
     )
 
 max_shwr_col = trimmed_showers_00km[:, np.argmax(mean_00km)].T
@@ -42,7 +42,7 @@ plt.hist(
     alpha=.5, 
     edgecolor='black', linewidth=.5,
     label='{:g} g/cm^2'.format(max_dpth_col[0]), 
-    bins = 100
+    bins = 30 
     )
 # plt.scatter(bin_ctr, freq, c='k') 
 plt.title('Distribution of Composite values')
@@ -54,40 +54,81 @@ plt.legend()
 
 #%% Dart board monte carlo
 n = 10
-bins = 20
+bins = 30
 
 
+def mc_drt_rms (n, bins, col_depths, col_showers):
+    freq, bin_edgs = np.histogram(col_showers, bins=bins)
+    bin_ctr = 0.5*(bin_edgs[1:] +bin_edgs[:-1])
+    bin_size = bin_ctr[1] - bin_ctr[0]
+    
+    
+    #get random values within the plotting window
+    rdom_x_ax = np.random.uniform(low=0.0, high=(8e7), size=n)
+    rdom_y_ax = np.random.uniform(low=0.0, high=(np.max(freq)+2), size=n)
+    
+    
+    plt.figure(figsize=(8,6),dpi=200) 
+    plt.hist(
+        col_showers, 
+        alpha=.5, 
+        edgecolor='black', linewidth=.5,
+        label=r'${:g} g/cm^2$'.format(col_depths[0]), 
+        bins = bins
+        )
+    plt.scatter(bin_ctr, freq, s=2, c='k') 
+    plt.scatter(rdom_x_ax, rdom_y_ax, s=2, c='r')
+    plt.xlim(right=8e7)
+    plt.ylim(top=np.max(freq)+2)
+    plt.title('bins = {}, n = {}'.format(bins,n))
+    plt.legend()
+    
+    x_residuals = np.abs(rdom_x_ax - bin_ctr[:, np.newaxis]) 
+    clst_x_idx = np.argmin(x_residuals, axis=0)#x_residuals < bin_size
+    smlst_resid = np.take_along_axis(x_residuals.T, clst_x_idx[:,None], axis=1) 
+    within_bin_msk = smlst_resid < bin_size
+    
+    test = clst_x_idx[:,None][within_bin_msk]
+    #print(test) 
+    accepted_bin_idxs = np.unique(clst_x_idx[:,None][within_bin_msk]) 
+    hit_y_axis_values = freq[accepted_bin_idxs]
+    #print(accepted_bin_idxs,hit_y_axis_values )
+    
+    # brute force loop, not sure how to vectorize yet
+    accepted_his_idxs = [] 
+    accepted_his_vals = []
+    for y_dart,bin_idx in zip(rdom_y_ax,test):
+        
+        if y_dart - freq[bin_idx] < 0: 
+            accepted_his_idxs.append(bin_idx)
+            #print(freq[bin_idx]) 
+            accepted_his_vals.append(freq[bin_idx])
+            
+    # accepted_his_idxs = np.unique(accepted_his_idxs) 
+    # accepted_his_vals = np.unique(accepted_his_vals)
+    #filtered_freq = freq[accepted_his_idxs] 
+    #print(accepted_his_idxs, filtered_freq) 
+    
+    plt.figure(figsize=(8,6),dpi=200) 
+    new_frequencies = np.zeros(np.size(freq)) 
+    new_frequencies[accepted_his_idxs] = accepted_his_vals
+    plt.bar(
+        bin_ctr, 
+        new_frequencies, 
+        bin_size,
+        alpha=.5, 
+        edgecolor='black', 
+        linewidth=.5,
+        label=r'${:g} g/cm^2$ MC Reconstructed'.format(col_depths[0]), 
+        ) 
+    plt.scatter(rdom_x_ax, rdom_y_ax, s=2, c='r')
+    
+    plt.title('bins = {}, n = {}'.format(bins,n))
+    plt.xlim(right=8e7)
+    plt.ylim(top=np.max(freq)+2)
+    plt.legend()
 
-freq, bin_edgs = np.histogram(max_shwr_col, bins=bins)
-bin_ctr = 0.5*(bin_edgs[1:] +bin_edgs[:-1])
-bin_size = bin_ctr[1] - bin_ctr[0]
-
-
-#get random values within the plotting window
-rdom_y_ax = np.random.uniform(low=0.0, high=(np.max(freq)+2), size=n)
-rdom_x_ax = np.random.uniform(low=0.0, high=(8e7), size=n)
-
-plt.figure(figsize=(8,6),dpi=200) 
-plt.hist(
-    max_shwr_col, 
-    alpha=.5, 
-    edgecolor='black', linewidth=.5,
-    label='{:g} g/cm^2'.format(max_dpth_col[0]), 
-    bins = bins
-    )
-plt.scatter(bin_ctr, freq, s=2, c='k') 
-plt.scatter(rdom_x_ax, rdom_y_ax, s=2, c='r')
-plt.xlim(right=8e7)
-plt.ylim(top=np.max(freq)+2)
-
-x_residuals = np.abs(rdom_x_ax - bin_ctr[:, np.newaxis]) 
-clst_x_idx = np.argmin(x_residuals, axis=0)#x_residuals < bin_size
-smlst_resid = np.take_along_axis(x_residuals.T, clst_x_idx[:,None], axis=1) 
-within_bin_msk = smlst_resid < bin_size
-
-accepted_bin_idxs = np.unique(clst_x_idx[:,None][within_bin_msk]) 
-
-
+mc_drt_rms(100, 100, col_depths=max_dpth_col, col_showers=max_shwr_col)
 
 
 
