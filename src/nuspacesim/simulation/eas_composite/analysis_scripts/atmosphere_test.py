0896d3f1b56c692217fff3074e1ff2d172111b33
Fred <fgarcia4@umd.edu>
2022-04-07 01:24:54 -0400

    atm test

--- src/nuspacesim/simulation/eas_composite/analysis_scripts/atmosphere_test.py
+++ src/nuspacesim/simulation/eas_composite/analysis_scripts/atmosphere_test.py
@@ -6,7 +6,7 @@ from nuspacesim.simulation.eas_optical.atmospheric_models import (
 import numpy as np
 import matplotlib.pyplot as plt
 
-altitudes = np.linspace(0, 25, 50)  # km
+altitudes = np.linspace(0, 20, 50)  # km
 atm_dens = cummings_atmospheric_density(altitudes)
 
 plt.figure(figsize=(8, 6), dpi=200)
@@ -14,22 +14,33 @@ plt.scatter(altitudes, atm_dens)
 plt.ylabel("Atm Density (g/cm^3)")
 plt.xlabel("Altitutde (km)")
 
+
 plt.figure(figsize=(8, 6), dpi=200)
 angles = np.radians(np.array([75, 80, 85, 87, 89]))  # np.radians(np.linspace(0, 80, 5))
 
+slant_depths_per_angle = []
 
 for angle in angles:
-    slant_depths = []
-    for i, alt in enumerate(altitudes[1:]):
 
+    slant_depths = []
+
+    for i, alt in enumerate(altitudes):
         g_cm2 = slant_depth(alt, 10000, angle)
         slant_depths.append(g_cm2)
 
-    slt_dpths = np.array(slant_depths)
+    slt_dpths = np.array(slant_depths)[:, 0]
+
+    slant_depths_per_angle.append(slt_dpths)
+
+slant_depths_per_angle = np.array(slant_depths_per_angle)
+
+
+for i, dpth in enumerate(slant_depths_per_angle):
+
     plt.plot(
-        altitudes[0:],
-        slt_dpths[:, :1],
-        label="{:.2f} degrees".format(np.degrees(angle)),
+        altitudes,
+        dpth,
+        label=r"{:.2f}$\degree$".format(np.degrees(angles[i])),
     )
 
 plt.title("track and earth zenith")
