3a4fcb0c5d6a16f7b97c696f488fab4819975203
Alex Reustle <areustledev@gmail.com>
2022-06-13 13:42:05 -0400

    Fixed mislabeled plot: tau_pexit_hist

--- src/nuspacesim/utils/plots.py
+++ src/nuspacesim/utils/plots.py
@@ -153,10 +153,10 @@ def tau_lorentz(sim, fig, ax):
     )
 
 
-def tau_pexit_hist(sim, fig, ax):
+def tau_pexit_hist(sim, _, ax):
     ax.hist(sim["tauExitProb"], 100, log=True, facecolor="g")
     ax.set_ylabel("Counts")
-    ax.set_xlabel(r"$\log(P_\mathrm{exit}(\tau))$")
+    ax.set_xlabel(r"$\P_\mathrm{exit}(\tau)$")
 
 
 def tau_pexit_density(sim, fig, ax):
